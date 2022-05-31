ea099db1b34158bc390ca2f3926ee5e60892e3c5
Ricardo <rguerrasantana@gmail.com>
2022-06-01 00:16:36 +1000

    🚧 Working on server.js

--- MyNotes-Backend/src/server.js
+++ MyNotes-Backend/src/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import bodyParser from "body-parser"
 
 const app = express()
 
+app.use(bodyParser.json())
+
 app.get("/hello", (req, res) => res.send("Hello!"))
-app.post("/hello", (req, res) => res.send("Hello!"))
+app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}!`))
 
 const port = 8000
 app.listen(port, () => {console.log(`Listening on port ${port}`)})
